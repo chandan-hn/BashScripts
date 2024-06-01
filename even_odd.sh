b041d3a40f6f17a00d7a11c8b3d72d4c9ef02422
chandan-hn <121455237+chandan-hn@users.noreply.github.com>
2024-06-02 00:31:18 +0530

    Update even_odd.sh

--- even_odd.sh
+++ even_odd.sh
@@ -6,11 +6,8 @@
 # Purpose of the script : "Write a shell script that accepts a positive integer as argument and checks if the number is odd or even"
 # Version : V1
 ##########################################
-if [[ $1 -gt 0 ]]; then
-	echo "You have entered a Non-negetive number"
-	if [[ $(($1 % 2 == 0)) ]]; then
-		echo "$1 is an even number"
-	fi
+if [[ $(($1 % 2)) -eq 0 ]]; then
+  echo "$1 is an even number"
 else
-	echo "$1 is an odd number"
+  echo "$1 is an odd number"
 fi
